b49f855674ca0effa9af62729f2ac898d563490d
john <jonmarinelli@hotmail.com>
2014-04-30 19:08:58 -0400

    added stock and weather yql query. installed node-mysql. updated index.jade

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var http = require('http');
 var express = require('express');
 var jade = require('jade');
+var YQL = require('yql');
+var mysql = require('mysql');
 
+/*
+*  express configurations
+*/
 var app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -14,8 +19,56 @@ var config = {
         "DATABASE" : "",
 };
 
+/*
+*  connect to mysql table
+*/ 
+var connection = mysql.createConnection({
+	host: '',
+	user: '',
+	password: '',
+	database: '',
+});
+
+connection.connect();
+connection.query('select * 
+/*
+*  get weather information for sunnyvale, CA (Yahoo's HQ)
+*/
+
+var weatherInfo = {
+	loc: '',
+	cond: '',
+	temp: 0,
+};
+
+new YQL.exec("SELECT * FROM weather.forecast WHERE (location = @zip)", function(response){
+	weatherInfo.loc = response.query.results.channel.location.city;
+	weatherInfo.temp = parseFloat(response.query.results.channel.item.condition.temp);
+	weatherInfo.cond = weatherInfo.temp + ' degrees; ' + response.query.results.channel.item.condition.text;
+
+	console.log(weatherInfo.loc);
+	console.log(weatherInfo.cond);
+}, {"zip": 85282});
+
+/*
+*  get stock information for Yahoo
+*/
+
+var stockInfo = {
+	dayHigh: 0,
+	dayLow: 0,
+};
+
+new YQL.exec("SELECT * FROM yahoo.finance.quote where symbol='yhoo'", function(response){
+	stockInfo.dayHigh = parseFloat(response.query.results.quote.DaysHigh);
+	stockInfo.dayLow = parseFloat(response.query.results.quote.DaysLow);
+});
+
+/*
+*  routing
+*/
 app.get('/', function(req, res){
-    res.render('index');
+    res.render('index', { weather: {loc: weatherInfo.loc, cond: weatherInfo.cond}, stock: {high: stockInfo.dayHigh, low: stockInfo.dayLow} });
 });
 
 app.use(function(err, req, res, next){
@@ -27,6 +80,9 @@ app.use(function(err, req, res, next){
     }
 });
 
+/*
+*  go node go
+*/
 var server = app.listen(3000, function(){
 	console.log('listening on port %d', server.address().port);
 });
