d4b978766cac5873327b01dd7657d154f8ca6a8c
john <jonmarinelli@hotmail.com>
2014-05-01 16:29:27 -0400

    removed accidental stuff from app.js

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ var config = {
 *  connect to mysql table
 */ 
 var connection = mysql.createConnection({
-	host: 'lol',
-	user: 'wut',
+	host: 'localhost',
+	user: 'administrator',
 	password: '',
-	database: '',
+	database: 'test',
 });
 
 connection.connect();
@@ -117,59 +117,4 @@ app.use(function(err, req, res, next){
 var server = app.listen(3000, function(){
 	console.log('listening on port %d', server.address().port);
 });
-connection.query('select * 
-/*
-*  get weather information for sunnyvale, CA (Yahoo's HQ)
-*/
 
-var weatherInfo = {
-	loc: '',
-	cond: '',
-	temp: 0,
-};
-
-new YQL.exec("SELECT * FROM weather.forecast WHERE (location = @zip)", function(response){
-	weatherInfo.loc = response.query.results.channel.location.city;
-	weatherInfo.temp = parseFloat(response.query.results.channel.item.condition.temp);
-	weatherInfo.cond = weatherInfo.temp + ' degrees; ' + response.query.results.channel.item.condition.text;
-
-	console.log(weatherInfo.loc);
-	console.log(weatherInfo.cond);
-}, {"zip": 85282});
-
-/*
-*  get stock information for Yahoo
-*/
-
-var stockInfo = {
-	dayHigh: 0,
-	dayLow: 0,
-};
-
-new YQL.exec("SELECT * FROM yahoo.finance.quote where symbol='yhoo'", function(response){
-	stockInfo.dayHigh = parseFloat(response.query.results.quote.DaysHigh);
-	stockInfo.dayLow = parseFloat(response.query.results.quote.DaysLow);
-});
-
-/*
-*  routing
-*/
-app.get('/', function(req, res){
-    res.render('index', { weather: {loc: weatherInfo.loc, cond: weatherInfo.cond}, stock: {high: stockInfo.dayHigh, low: stockInfo.dayLow} });
-});
-
-app.use(function(err, req, res, next){
-    if(req.xhr){
-        res.send(500, 'something is wrong');
-    }
-    else{
-        next(err);
-    }
-});
-
-/*
-*  go node go
-*/
-var server = app.listen(3000, function(){
-	console.log('listening on port %d', server.address().port);
-});
