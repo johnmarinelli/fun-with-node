08410efe25237b876fd2286ba62630ad22d1a23f
john <jonmarinelli@hotmail.com>
2014-05-03 01:40:16 -0400

    started working on user input, refactored

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var http = require('http');
 var express = require('express');
 var jade = require('jade');
-var YQL = require('yql');
 var mysql = require('mysql');
+var utility = require('utility');
 
 /*
 *  express configurations
 */
 var app = express();
+
+app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
@@ -31,55 +33,20 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-/*
-*  connect to mysql table
-*/ 
-var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'administrator',
-	password: '',
-	database: 'test',
-});
-
-connection.connect();
-/*
-*  get weather information for sunnyvale, CA (Yahoo's HQ)
-*/
-
-var weatherInfo = {
-	loc: '',
-	cond: '',
-	temp: 0,
-};
-
-new YQL.exec("SELECT * FROM weather.forecast WHERE (location = @zip)", function(response){
-	weatherInfo.loc = response.query.results.channel.location.city;
-	weatherInfo.temp = parseFloat(response.query.results.channel.item.condition.temp);
-	weatherInfo.cond = weatherInfo.temp + ' degrees; ' + response.query.results.channel.item.condition.text;
-
-	console.log(weatherInfo.loc);
-	console.log(weatherInfo.cond);
-}, {"zip": 85282});
-
-/*
-*  get stock information for Yahoo
-*/
-
-var stockInfo = {
-	dayHigh: 0,
-	dayLow: 0,
-};
-
-new YQL.exec("SELECT * FROM yahoo.finance.quote where symbol='yhoo'", function(response){
-	stockInfo.dayHigh = parseFloat(response.query.results.quote.DaysHigh);
-	stockInfo.dayLow = parseFloat(response.query.results.quote.DaysLow);
-});
+var weatherInfo = utility.weatherQuery(94089);
+var stockInfo = utility.stockQuery('yhoo');
 
 /*
 *  routing
 */
 app.get('/', function(req, res){
     res.render('index', { weather: {loc: weatherInfo.loc, cond: weatherInfo.cond}, stock: {high: stockInfo.dayHigh, low: stockInfo.dayLow} });
+});
+
+app.post('/getinput', function(req, res){
+	console.log(req);
+	console.log(req.body.weather);
+	res.send(req.body.weather);
 });
 
 app.use(function(err, req, res, next){
@@ -94,7 +61,7 @@ app.use(function(err, req, res, next){
 /*
 *  go node go
 */
-var server = app.listen(3000, function(){
+var server = app.listen(1337, function(){
 	console.log('listening on port %d', server.address().port);
 });
 
